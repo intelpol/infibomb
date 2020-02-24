4a5109ebeb37705770a3b57bbe03f9ea8e18248f
Debug <51507225+intelpol@users.noreply.github.com>
2020-02-24 22:52:54 +0300

    Add files via upload

---
+++ IB_arm64tor/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+chmod +x ./IBarm64tor
+cp ~/IBarm64tor/infinite-bomber /data/data/com.termux/files/usr/bin/infinite-bomber
+cp ~/IBarm64tor/infinite-bomber /data/data/com.termux/files/usr/bin/infinite
+cp ~/IBarm64tor/services.yaml /data/data/com.termux/files/usr/bin/services.yaml
