ee765b9403972c3721ec577d1bf7a7d67315d7f6
Debug <51507225+intelpol@users.noreply.github.com>
2020-02-24 23:31:30 +0300

    Update install.sh

--- IB_arm64tor/install.sh
+++ IB_arm64tor/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chmod +x ./IBarm64tor
+chmod +x ./IB_arm64tor
 cp ~/IB_arm64tor/infinite-bomber /data/data/com.termux/files/usr/bin/infinite-bomber
 cp ~/IB_arm64tor/infinite-bomber /data/data/com.termux/files/usr/bin/infinite
 cp ~/IB_arm64tor/services.yaml /data/data/com.termux/files/usr/bin/services.yaml
