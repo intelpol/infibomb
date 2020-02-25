fd929e4bb8d5f8c13c4cab2bbe7b593a5696502f
Debug <51507225+intelpol@users.noreply.github.com>
2020-02-25 16:25:45 +0300

    Delete install.sh

--- IB_arm64tor/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-chmod +x ./IB_arm64tor
-cp ~/IB_arm64tor/infinite-bomber /data/data/com.termux/files/usr/bin/infinite-bomber
-cp ~/IB_arm64tor/infinite-bomber /data/data/com.termux/files/usr/bin/infinite
-cp ~/IB_arm64tor/services.yaml /data/data/com.termux/files/usr/bin/services.yaml
